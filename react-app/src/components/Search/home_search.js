39f1b4f190be99a340a0b870d0c7a68575d96d42
alice886 <baconhill4tmr@gmail.com>
2022-09-13 22:07:58 -0700

    restyle main-search

--- react-app/src/components/Search/home_search.js
+++ react-app/src/components/Search/home_search.js
@@ -11,29 +11,25 @@ export default function HomeSearch() {
     const todayMonth = d.getMonth() + 1
     const todayString = [d.getFullYear(), ('0' + todayMonth).slice(-2), ('0' + d.getDate()).slice(-2)].join('-')
     const nowHour = d.getHours();
-    const availableHour_count = []
 
     const [partySize, setPartySize] = useState(2);
-    const [searchTime, setSearchTime] = useState(('0' + (nowHour + 1)).slice(-2) + ':00');
+    const [searchTime, setSearchTime] = useState('12:00');
+    // const [searchTime, setSearchTime] = useState(('0' + (nowHour + 1)).slice(-2) + ':00');
     const [searchDate, setSearchDate] = useState(todayString);
     const [keyWord, setKeyWord] = useState()
+
+    const availableHour_count = []
+    for (let i = 0; i < 24; i++) {
+        availableHour_count.push(('0' + i).slice(-2) + ':00')
+        // availableHour_count.push(('0' + i).slice(-2) + ':30')
+    }
+
 
     const capacity_count = []
     for (let i = 1; i < 21; i += 1) {
         capacity_count.push(i)
     }
 
-    if (new Date(searchDate) > d) {
-        for (let i = 0; i < 24; i++) {
-            availableHour_count.push(('0' + i).slice(-2) + ':00')
-        }
-    }
-    else {
-        for (let i = nowHour + 1; i < 24; i++) {
-            availableHour_count.push(i + ':00')
-
-        }
-    }
 
     return (
         <div className='home-search-container'>
@@ -49,12 +45,9 @@ export default function HomeSearch() {
             </div>
             <div>
                 <select className='home-search-dattimeppl' value={searchTime} onChange={e => setSearchTime(e.target.value)} required >
-                    {(availableHour_count.length > 0) ?
-                        availableHour_count.map(each => {
-                            return <option value={each} key={each} onClick={e => setSearchTime(e.target.value)}>{each}</option>
-                        })
-                        : (<option value={''} selected disabled hidden>* No available time on the selected date</option>)
-                    }
+                    {availableHour_count.map(each => {
+                        return <option value={each} key={each} onClick={e => setSearchTime(e.target.value)}>{each}</option>
+                    })}
                 </select>
             </div>
             <div>
