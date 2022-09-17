ed11d32a81fbef046e6da5713554cf9f5f90add7
alice886 <baconhill4tmr@gmail.com>
2022-09-16 23:33:43 -0700

    search testing

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -10,22 +10,10 @@ from .auth_routes import validation_errors_to_error_messages
 search_routes = Blueprint('search', __name__)
 
 @search_routes.route('/', methods=['GET'])
-@search_routes.route('/', methods=['GET'])
+@search_routes.route('', methods=['GET'])
 def home_search():
-    form = SearchForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    # search_date = form.data['search_date']
-    # search_time = form.data['search_time']
-    if form.data['key_word']:
-        key_word = set(form.data['key_word'].split(' '))
-    else:
-        key_word = form.data['key_word']
-    print(key_word)
-    return {'what would be returned': form.data['key_word']}
-    # print(set(key_word.split(' ')))
-    # return {each for each in key_word}
-
-
+    return {'url is': request.args.get('keyword')}
+    # return {'url is': request.full_path}
 
     # restaurants = db.session.query(Restaurant).all()
     # return {'restaurants': [restaurant.to_dict() for restaurant in restaurants]}
