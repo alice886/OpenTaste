2a3ebda9a3e20ac20ff3991fda3e2b054efb840e
alice886 <baconhill4tmr@gmail.com>
2022-09-22 22:27:54 -0700

    added search page

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
+from flask_paginate import Pagination, get_page_parameter
+# from flask_paginate import Pagination
 from sqlalchemy.orm import joinedload, Load, subqueryload
 from app.models import db, Restaurant
 # from app.forms import SearchForm
@@ -12,8 +14,12 @@ search_routes = Blueprint('search', __name__)
 @search_routes.route('/', methods=['GET'])
 @search_routes.route('', methods=['GET'])
 def home_search():
+    restaurants = db.session.query(Restaurant).all()
+    # page = request.args.get(get_page_parameter(), type= int, default=1)
+    # pagination = Pagination(page=page, total= restaurant.count(), search=False)
+    return {'restaurants': [restaurant.to_dict() for restaurant in restaurants]}
     # http://localhost:3000/api/search?dateTime=2022-09-17T12%3A00%3A00&covers=2&term=san%20mateo
-    return {'url is': request.args.get('dateTime')} #returns '2022-09-17T12:00:00'
+    # return {'url is': request.args.get('dateTime')} #returns '2022-09-17T12:00:00'
     # return {'url is': request.args.get('covers')}  # returns '2'
     # return {'url is': request.args.get('term')} # returns 'san mateo'
     # return {'url is': request.full_path}
--- react-app/src/components/Search/search_page.js
+++ react-app/src/components/Search/search_page.js
@@ -1,23 +1,89 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMyRestaurantThunk } from '../../store/restaurant';
-import { getMyReservationsThunk } from '../../store/reservation';
+import { searchRestaurantThunk } from '../../store/search';
 import { NavLink, Redirect, useHistory, useParams } from "react-router-dom";
-import GoogleMapAPI from '../Gmap/gmap'
+import defaultImg3 from '../../icons/defaultImg3.png'
+import './search_page.css'
 
 export default function SearchPage() {
     const history = useHistory();
     const dispatch = useDispatch();
+    const [loaded, setLoaded] = useState(false);
+    const [showHomeReserve, setShowHomeReserve] = useState(false);
+    const [resId, setRestId] = useState();
+    const [resTime, setResTime] = useState();
+    const searchRes = useSelector(state => state.search)
+
+    const dollarSigns = ['', '$', '$$', '$$$', '$$$$'];
+
+    useEffect(() => {
+        dispatch(searchRestaurantThunk()).then(() => setLoaded(true))
+    }, [dispatch])
 
     const { dateTime, covers, term } = useParams();
-    console.log(dateTime)
-    console.log(covers)
-    console.log(term)
+    // console.log(dateTime)
+    // console.log(covers)
+    // console.log(term)
+
+    const handleHomeReserve = async (e, id) => {
+        e.preventDefault();
+        setRestId(id)
+        setResTime(e.target.value)
+        setShowHomeReserve(true)
+    }
+
+    let d = new Date()
+    // d = new Date(d.toLocaleString('en-US', { timeZone: 'America/Los_Angeles' }))
+    const todayMonth = d.getMonth() + 1
+    const todayString = [d.getFullYear(), ('0' + todayMonth).slice(-2), ('0' + d.getDate()).slice(-2)].join('-')
+    const nowHour = d.getHours();
+    const getHours = each => {
+        return Number(each['close_time'].slice(0, 2));
+    }
 
 
-    return (
-        <>
-            <h2>now you are in search page</h2>
-        </>
+    return loaded && (
+        <div>
+            <div className='search-all-container'>
+                <div className='search-left'>
+                    column for map & filters
+                </div>
+                <div className='search-right'>
+                    <div className='search-right-sort'>
+                        Sort v
+                    </div>
+                    <div className='search-right-res'>
+                        {searchRes?.map(restaurant => {
+                            return <div className='search-res-each' key={restaurant.id}>
+                                <NavLink className='search-res-nav-whole' to={`/restaurants/${restaurant.id}`}>
+                                    <div className='search-res-cover'>
+                                        <img src={restaurant.cover} alt='restaurant img' height={'200px'}
+                                            onError={(e) => {
+                                                if (e.target.src !== defaultImg3) { e.target.onerror = null; e.target.src = defaultImg3; }
+                                            }} />
+                                    </div>
+                                    <div className='search-res-details'>
+                                        <div className='search-res-nav-div'>
+                                            <NavLink className='search-res-nav' to={`/restaurants/${restaurant.id}`}>{restaurant.name}</NavLink>
+                                        </div>
+                                        <div className='search-res-dcl'>{dollarSigns[restaurant.price_range]} · {restaurant.cuisine} · {restaurant.city}</div>
+                                        <div className='search-res-timeslots'>
+                                            <button onClick={e => handleHomeReserve(e, restaurant.id)} value={getHours(restaurant) - 3} disabled={getHours(restaurant) - 3 <= nowHour}>{getHours(restaurant) - 3}:00</button>
+                                            <button onClick={e => handleHomeReserve(e, restaurant.id)} value={getHours(restaurant) - 2} disabled={getHours(restaurant) - 2 <= nowHour}>{getHours(restaurant) - 2}:00</button>
+                                            <button onClick={e => handleHomeReserve(e, restaurant.id)} value={getHours(restaurant) - 1} disabled={getHours(restaurant) - 1 <= nowHour}>{getHours(restaurant) - 1}:00</button>
+                                        </div>
+                                    </div>
+                                </NavLink>
+
+                                {/* {restaurant => slotGenerator(restaurant).map(each => {
+                            <div>{each}</div>
+                        })} */}
+                            </div >
+                        })
+                        }
+                    </div>
+                </div>
+            </div >
+        </div>
     )
 }
