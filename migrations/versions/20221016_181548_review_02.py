c7befb5613ae511e3929c5b3d11a737be436272c
alice886 <baconhill4tmr@gmail.com>
2022-10-16 18:17:00 -0700

    review02

--- migrations/versions/20221016_181548_review_02.py
+++ migrations/versions/20221016_181548_review_02.py
@@ -1,8 +1,8 @@
-"""empty message
+"""review-02
 
-Revision ID: 9cf9424da977
+Revision ID: 06089cc461f6
 Revises: 
-Create Date: 2022-10-14 12:02:51.710898
+Create Date: 2022-10-16 18:15:48.236154
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9cf9424da977'
+revision = '06089cc461f6'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -84,7 +84,7 @@ def upgrade():
     sa.Column('service', sa.Integer(), nullable=False),
     sa.Column('ambience', sa.Integer(), nullable=False),
     sa.Column('overall', sa.Integer(), nullable=False),
-    sa.Column('review_body', sa.String(length=200), nullable=True),
+    sa.Column('review_body', sa.String(length=500), nullable=True),
     sa.Column('created_at', sa.DateTime(), nullable=False),
     sa.Column('updated_at', sa.DateTime(), nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=False),
